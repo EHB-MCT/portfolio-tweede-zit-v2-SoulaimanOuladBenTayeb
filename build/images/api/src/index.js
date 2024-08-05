b7a28f136ae769f2514bb26e8c27104a4f50aa2c
souli <soulaiman.oulad.ben.tayeb@student.ehb.be>
2024-08-06 01:08:19 +0200

    First step. Install Express, create a Docker container. Make the structure of the folder (gitignore, dockerignore).

---
+++ build/images/api/src/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (request,response ) =>{
+    response.send({message: "hellooo worldsss"})
+})
+
+app.listen(3000, (err) => {
+    if(!err) {
+        console.log("running on port " + 3000);
+    }
+    else {
+        console.error(err)
+    }
+
+})
